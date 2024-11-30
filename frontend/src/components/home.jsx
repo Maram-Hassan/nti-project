fa7ab838c5d5e99fd3820ac1eaab79cee1d7b1d9
Maram-Hassan <maram.hassan95@gmail.com>
2024-11-30 19:50:40 +0200

    first commit

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -14,6 +14,7 @@ import {
 import TodoForm from "./todo-form";
 
 const API_URL = process.env.REACT_APP_API_URL;
+console.log('API_URL:', API_URL);
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
@@ -47,7 +48,7 @@ const Home = () => {
 
   const handleNewTodo = async (todo) => {
     try {
-      await axios.post(`${API_URL}/todos`, todo);
+      await axios.post(`http://localhost:3001/api/todos`, todo);
       await getTodos();
       setModalOpen(false);
     } catch (err) {
